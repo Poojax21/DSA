834eb6171446a239c099aa87b009acc8a156144b
Pooja Wavdara <poojax21@gmail.com>
2026-03-20 23:25:12 +0530

    DSA

---
+++ Array/buyandsellstocks.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int maxProfit = 0;
+        int n = prices.size();
+
+        for(int i = 0; i < n; i++) {
+            int cost = prices[i] - mini;
+            maxProfit = max(maxProfit, cost);
+            mini = min(mini, prices[i]);
+        }
+
+        return maxProfit;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter number of days: ";
+    cin >> n;
+
+    vector<int> prices(n);
+    cout << "Enter prices: ";
+    for(int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+
+    Solution obj;
+    int result = obj.maxProfit(prices);
+
+    cout << "Maximum Profit: " << result << endl;
+
+    return 0;
+}
